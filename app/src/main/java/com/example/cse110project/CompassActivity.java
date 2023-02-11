33194cfeceff7d8c42e02338e916ac764839d1d5
Taylor Salazar <tesalazar@ucsd.edu>
2023-02-11 15:47:52 -0800

    Added CompassUI and House Blips

---
+++ app/src/main/java/com/example/cse110project/CompassActivity.java
@@ -0,0 +1,16 @@
+package com.example.cse110project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class CompassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compass);
+    }
+
+}
