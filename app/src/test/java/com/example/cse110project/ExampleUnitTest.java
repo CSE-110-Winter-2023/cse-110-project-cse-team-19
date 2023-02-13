67d16d71b4172c43c384f7be26dbb58d95126e55
Tyler-Culp <tculp@ucsd.edu>
2023-02-12 23:21:29 -0800

    added robolectric to test file

--- app/src/test/java/com/example/cse110project/ExampleUnitTest.java
+++ app/src/test/java/com/example/cse110project/ExampleUnitTest.java
@@ -1,6 +1,12 @@
 package com.example.cse110project;
 
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
@@ -9,6 +15,8 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
+@RunWith(AndroidJUnit4.class)
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
