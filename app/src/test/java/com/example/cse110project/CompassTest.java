79ac553dab823c4b1aeba3f27cad1fba4e326c69
richard21a <97638935+richard21a@users.noreply.github.com>
2023-02-13 00:09:32 -0800

    added UI testing

---
+++ app/src/test/java/com/example/cse110project/CompassTest.java
@@ -0,0 +1,33 @@
+package com.example.cse110project;
+
+import static org.junit.Assert.assertEquals;
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+import androidx.lifecycle.Lifecycle;
+
+import androidx.test.core.app.ActivityScenario;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricTestRunner.class)
+public class CompassTest {
+    @Config(sdk = 31)
+    @Test
+    public void test_UI_exists(){
+        var scenario = ActivityScenario.launch(CompassActivity.class);
+        scenario.onActivity(activity -> {
+            ImageView compass = (ImageView) activity.findViewById(R.id.compass_image);
+            ImageView redIcon = (ImageView) activity.findViewById(R.id.red_icon);
+            //assertEquals(0,0);
+
+            assertEquals(View.VISIBLE, compass.getVisibility());
+            assertEquals(View.VISIBLE, compass.getVisibility());
+        });
+        //ImageView compass = findViewById(R.id.compass_image);
+    }
+}
