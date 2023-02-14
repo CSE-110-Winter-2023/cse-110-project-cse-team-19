3c8154d0f8ead26e3c46f31fab67b4e32198c48d
elaine <elchao@ucsd.edu>
2023-02-14 14:58:02 -0800

    merged main and loc and orient

--- app/src/main/java/com/example/cse110project/MainActivity.java
+++ app/src/main/java/com/example/cse110project/MainActivity.java
@@ -12,14 +12,34 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    private LocationService locationService;
+    private OrientationService orientationService;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void getLocAndOrientActivity(View view) {
-        Intent intent = new Intent(this, LocAndOrientActivity.class);
-        startActivity(intent);
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 200);
+        }
+
+        locationService = LocationService.singleton(this);
+
+
+        TextView textView = (TextView) findViewById(R.id.serviceTextView);
+
+        locationService.getLocation().observe(this, loc->{
+            textView.setText(Double.toString(loc.first) + " , " +
+                    Double.toString(loc.second));
+        });
+
+        orientationService = OrientationService.singleton(this);
+        TextView orientationView = (TextView) findViewById(R.id.orientationView);
+        orientationService.getOrientation().observe(this, orientation->{
+            orientationView.setText(Float.toString(orientation));
+        });
     }
 }
