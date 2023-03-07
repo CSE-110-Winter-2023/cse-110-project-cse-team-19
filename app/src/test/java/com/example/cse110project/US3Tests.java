5246ed51df25c665e44fcf30cc8b0692b6957e56
Tyler-Culp <tculp@ucsd.edu>
2023-03-07 01:43:52 -0800

    Added a user class which can be used as the template for what we get from and store to the database

---
+++ app/src/test/java/com/example/cse110project/US3Tests.java
@@ -0,0 +1,37 @@
+package com.example.cse110project;
+
+import static android.content.Context.MODE_PRIVATE;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+
+import android.Manifest;
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ImageView;
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.platform.app.InstrumentationRegistry;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.Shadows;
+import org.robolectric.shadows.ShadowApplication;
+import java.time.Instant;
+
+@RunWith(RobolectricTestRunner.class)
+
+public class US3Tests {
+
+    @Test
+    public void getTimeTest() {
+        String currTime = Instant.now().toString();
+        assertNotNull(currTime);
+        System.out.println("\n" + currTime + "\n");
+    }
+}
