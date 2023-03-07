efb65be22608ca53a527db950802462ebcf04f56
Tyler-Culp <tculp@ucsd.edu>
2023-03-07 02:00:37 -0800

    finished UserDatabase, may have one bug in there

--- app/src/main/java/com/example/cse110project/UserDatabase.java
+++ app/src/main/java/com/example/cse110project/UserDatabase.java
@@ -11,4 +11,27 @@ import androidx.room.RoomDatabase;
 public abstract class UserDatabase extends RoomDatabase {
     private volatile static UserDatabase instance = null;
     public abstract UserDao getDao();
+
+    public synchronized static UserDatabase provide(Context context) {
+        if (instance == null) {
+            instance = UserDatabase.make(context);
+        }
+        return instance;
+    }
+
+    private static UserDatabase make(Context context) {
+        // TODO: Check whether or not user_app.db is actually what should be there
+        return Room.databaseBuilder(context, UserDatabase.class, "user_app.db")
+                .allowMainThreadQueries()
+                .fallbackToDestructiveMigration()
+                .build();
+    }
+
+    @VisibleForTesting
+    public static void inject(UserDatabase testDatabase) {
+        if (instance != null) {
+            instance.close();
+        }
+        instance = testDatabase;
+    }
 }
